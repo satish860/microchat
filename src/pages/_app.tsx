ba91da9b224aa818b17a2ed130acfee22f0fb78d
Satish Venkatakrishnan <satish860@gmail.com>
2023-03-19 11:03:48 +0530

    Login Provider with a login button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </UserProvider>
+  );
 }
